83317ec720eeee9cd70027a9ba8ba08ccc4c1f38
Jose Huby <jose.huby@utec.edu.pe>
2022-04-20 20:20:15 -0500

    Nueva version: insert y search listos

--- Set4Batches.hpp
+++ Set4Batches.hpp
@@ -47,6 +47,7 @@ class Set4Batches{
     struct Job{
         int subset;
         std::function<void(void)> f;
+        int from;
     };
 
     Pointer root;
@@ -222,7 +223,59 @@ class Set4Batches{
 
     void contains_t(std::vector<Key> const& keys, std::vector<KeyExistence>& result, int from, Pointer current){
         if(IS_NOT_LEAF(current) && keys.size() > 1){
+            std::vector<std::vector<Key>> subset(SIZE(current) + 1);
+            const int reservation = 2*(keys.size())/(SIZE(current) + 1);
+            for(auto& s : subset) s.reserve(reservation);
+            for(auto const& key : keys) {
+                int i = 0;
+                for(; i<SIZE(current); ++i){
+                    if(key < KEY(current, i)) break;
+                }
+                subset[i].push_back(key);
+            }
 
+            int sub_from = from;
+            std::vector<Job> jobs; jobs.reserve(SIZE(current) + 1);
+            for(int i = 0; i <= SIZE(current); ++i) if(subset[i].size() > 0) {
+                jobs.push_back({
+                    i, 
+                    [this, &subset, &result, sub_from, current, i]{
+                        this->contains_r(subset[i], result, sub_from, CHILD(current, i));
+                    }, 
+                    sub_from
+                });
+                sub_from += subset[i].size();
+            }
+            std::sort(jobs.begin(), jobs.end(), [&subset](Job const& a, Job const& b){return subset[a.subset].size() > subset[b.subset].size();});
+            
+            std::vector<std::thread> threads;
+            const int equality = keys.size() / free_workers;
+            while(free_workers && free_workers <= jobs.size()){
+                int acc = -1;
+                std::vector<std::function<void(void)>> worker_jobs;
+                while(acc < equality && free_workers <= jobs.size()){
+                    auto const& job = jobs.back();
+                    worker_jobs.push_back(job.f);
+                    acc += subset[job.subset].size();
+                    jobs.pop_back();
+                }
+                threads.emplace_back([worker_jobs]{for(auto const& worker_job : worker_jobs) worker_job();});
+                --free_workers;
+            }
+            if(jobs.size()) {
+                while(jobs.size() > 1){
+                    const auto worker_job = jobs.back().f;
+                    threads.emplace_back([worker_job]{worker_job();});
+                    jobs.pop_back();
+                    --free_workers;
+                }
+                const int i = jobs.back().subset;
+                sub_from = jobs.back().from;
+               threads.emplace_back([this, &subset, &result, sub_from, current, i]{
+                    this->contains_t(subset[i], result, sub_from, CHILD(current, i));
+                });
+            }
+            for(auto& t : threads) t.join();
         }else{
             contains_r(keys, result, from, current);
         }
@@ -252,8 +305,7 @@ class Set4Batches{
         }
         else {
             free_workers = workers;
-            contains_r(keys, result, 0, root);
-            //contains_t(keys, result, 0, root);
+            contains_t(keys, result, 0, root);
         }
         return result;
     }
